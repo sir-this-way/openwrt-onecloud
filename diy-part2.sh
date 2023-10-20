77bd291bc3baf663a86720a3fe2993f815421018
sir-this-way <146545517+sir-this-way@users.noreply.github.com>
2023-10-20 22:28:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,5 +16,5 @@
 # Modify default IP
 #!/bin/bash
 #=================================================
-sed -i "s/192.168.7.1/192.168.7.101/" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/192.168.7.101/" package/base-files/files/bin/config_generate
 sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite --force-depends/' package/Makefile
